dcba3d0605a2e6ad63441adbc4d8283e326bce27
Rona Cao <rcao26@gmail.com>
2023-11-08 21:44:31 +0800

    fixed merged function and merged dfs by each year

--- data-analysis/visualize.py
+++ data-analysis/visualize.py
@@ -1,25 +1,41 @@
-import pandas as pd
 import os
+import pandas as pd
 
 def merge_df():
-    # combine all dataframes into one
-    # find min and max times for time taken
-    # find min and max grade 
-    # plot 
+    # Combine all dataframes into one
     years = ["F20", "F21", "F22"]
     for year in years:
         dir_list = sorted(os.listdir(f"../data/clean/{year}/HW"))
-        df_total = pd.DataFrame(list()) # create empty csv file 
-        df_total.to_csv('merged_df.csv')
+        df_total = pd.DataFrame()  # Create an empty DataFrame
+
         for path in dir_list:
-            df = pd.read_csv(f"../data/clean/{year}/HW/{path}") # reading each file 
-            df_total = pd.concat([df_total, df], axis = 0)
+            df = pd.read_csv(f"../data/clean/{year}/HW/{path}")  # Read each file
+            df_total = pd.concat([df_total, df], axis=0)
+
         # Select specific columns in the DataFrame
-        df_total = df_total[['State', 'Started on','Completed','Time taken','Grade/20.00']]
+        df_total = df_total[['State', 'Started on', 'Completed', 'Time taken', 'Grade/20.00']]
 
-    df_total.to_csv('merged_df.csv', index=False)
+        # Write a separate CSV file for each year
+        output_file = f'merged_df_{year}.csv'
+        df_total.to_csv(output_file, index=False)
 
-   # data = pd.read_csv(f'data/clean/{year}/HW') # reading csv
+
+
+def time_taken_corr_grade():
+    df = pd.read_csv('merged_df.csv') 
+    time_taken = df[['Time taken']]
+    grade = df[['Grade/20.00']]
+
+    for time in time_taken:
+        min = time
+
+
+
+    # find min and max times for time taken
+    # find min and max grade 
+    # plot 
+
 
 if __name__ == "__main__":
 	merge_df()
+    #time_taken_corr_grade()
