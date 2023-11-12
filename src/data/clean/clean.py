cc75f888a765f05814e12ff016200572ffb12bde
163dgm <mgd361@nyu.edu>
2023-11-12 07:45:01 -0500

    reorganize repo file structure

--- src/data/clean/clean.py
+++ src/data/clean/clean.py
@@ -1,24 +1,19 @@
-import pandas as pd
 import os
+import pandas as pd
 import clean_utils as utils
 
-<<<<<<< Updated upstream
 years = ["F20", "F21", "F22"]
 
 def hw_clean():
 	"""
-	Takes each year's homework and removes unneeded columns, 
+	Takes each year's homework and removes unneeded columns,
 	removes each homework that was not finished, and converts
 	string times to minutes.
 	"""
-=======
-def clean():
-	years = ["F20", "F21", "F22"]
->>>>>>> Stashed changes
 	for year in years:
-		hw_folder = sorted(os.listdir(f"./raw/{year}/HW"))
+		hw_folder = sorted(os.listdir(f"../raw/{year}/HW"))
 		for hw_path in hw_folder:
-			raw_hw_path = f"./raw/{year}/HW/{hw_path}"
+			raw_hw_path = f"../raw/{year}/HW/{hw_path}"
 			raw_hw = pd.read_csv(raw_hw_path)
 
 			# Remove unneeded columns
@@ -32,20 +27,20 @@ def clean():
 			clean_hw['Time taken'] = clean_hw['Time taken'].apply(utils.str_time_to_minutes)
 
 			try:
-				os.makedirs(f"./clean/{year}/HW/")
+				os.makedirs(f"./{year}/HW/")
 			except:
 				None
 			finally:
-				clean_hw.to_csv(f"./clean/{year}/HW/{hw_path}", index=False)
+				clean_hw.to_csv(f"./{year}/HW/{hw_path}", index=False)
 
 def xlsx_clean():
 	"""
-	Splits `./raw/Moodle Datasets.xlsx` into individual sheets 
+	Splits `../raw/Moodle Datasets.xlsx` into individual sheets
 	and moves them into each respective year's folder.
 	"""
 	for year in years:
-		year_sheet = pd.read_excel("./raw/Moodle Datasets.xlsx", sheet_name=year)
-		year_sheet.to_csv(f"./clean/{year}/deadlines.csv", index=False)
+		year_sheet = pd.read_excel("../raw/Moodle Datasets.xlsx", sheet_name=year)
+		year_sheet.to_csv(f"./{year}/deadlines.csv", index=False)
 
 
 if __name__ == "__main__":
