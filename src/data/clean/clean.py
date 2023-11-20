abc37a5c3d9db8e67e0d12426bf1ae7a858874b4
163dgm <mgd361@nyu.edu>
2023-11-20 03:38:59 -0500

    Clean ME FE and LB

--- src/data/clean/clean.py
+++ src/data/clean/clean.py
@@ -4,44 +4,55 @@ import clean_utils as utils
 
 years = ["F20", "F21", "F22"]
 
-def hw_clean():
-	"""
-	Takes each year's homework and removes unneeded columns,
-	removes each homework that was not finished, and converts
-	string times to minutes.
-	"""
-	for year in years:
-		hw_folder = sorted(os.listdir(f"../raw/{year}/HW"))
-		for hw_path in hw_folder:
-			raw_hw_path = f"../raw/{year}/HW/{hw_path}"
-			raw_hw = pd.read_csv(raw_hw_path)
 
-			# Remove unneeded columns
-			removed_columns = ["Surname", "First name", "Email address"]
-			clean_hw = raw_hw.drop(columns=removed_columns)
+def clean(assgn: str):
+    """
+    Takes each year's homework and removes unneeded columns,
+    removes each homework that was not finished, and converts
+    string times to minutes.
+    """
+    for year in years:
+        assgn_folder = None
+        try:
+            assgn_folder = sorted(os.listdir(f"../raw/{year}/{assgn}"))
+        except:
+            continue
 
-			# Remove unfinished homework
-			clean_hw = clean_hw.drop(clean_hw[clean_hw["State"] != "Finished"].index)
+        for assgn_path in assgn_folder:
+            raw_assgn_path = f"../raw/{year}/{assgn}/{assgn_path}"
+            raw_assgn = pd.read_csv(raw_assgn_path)
 
-			# Convert string time taken to minutes
-			clean_hw['Time taken'] = clean_hw['Time taken'].apply(utils.str_time_to_minutes)
+            # Remove unneeded columns
+            removed_columns = ["Surname", "First name", "Email address"]
+            clean_assgn = raw_assgn.drop(columns=removed_columns)
 
-			try:
-				os.makedirs(f"./{year}/HW/")
-			except:
-				None
-			finally:
-				clean_hw.to_csv(f"./{year}/HW/{hw_path}", index=False)
+            # Remove unfinished homework
+            clean_assgn = clean_assgn.drop(
+                clean_assgn[clean_assgn["State"] != "Finished"].index
+            )
+
+            # Convert string time taken to minutes
+            clean_assgn["Time taken"] = clean_assgn["Time taken"].apply(
+                utils.str_time_to_minutes
+            )
+
+            try:
+                os.makedirs(f"./{year}/{assgn}/")
+            except:
+                None
+            finally:
+                clean_assgn.to_csv(f"./{year}/{assgn}/{assgn_path}", index=False)
+
 
 def xlsx_clean():
-	"""
-	Splits `../raw/Moodle Datasets.xlsx` into individual sheets
-	and moves them into each respective year's folder.
-	"""
-	for year in years:
-		year_sheet = pd.read_excel("../raw/Moodle Datasets.xlsx", sheet_name=year)
-		year_sheet.to_csv(f"./{year}/deadlines.csv", index=False)
+    """
+    Splits `../raw/Moodle Datasets.xlsx` into individual sheets
+    and moves them into each respective year's folder.
+    """
+    for year in years:
+        year_sheet = pd.read_excel("../raw/Moodle Datasets.xlsx", sheet_name=year)
+        year_sheet.to_csv(f"./{year}/deadlines.csv", index=False)
 
 
 if __name__ == "__main__":
-	pass
+    pass
