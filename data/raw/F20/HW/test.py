47200442da3023532e3a180fcd5ec37f4a909567
Rona Cao <rcao26@gmail.com>
2023-12-04 17:27:28 +0800

    created mark submission status of students in data/raw/F20/HW

--- data/raw/F20/HW/test.py
+++ data/raw/F20/HW/test.py
@@ -72,101 +72,160 @@ def clean_assignment_csv(df: pd.DataFrame):
     df["Grade/20.00"] = df["Grade/20.00"].astype("float")
 
     df = rename_columns(df)
+    df = df[
+        [
+            "Surname",
+            "First Name",
+            "Student ID",
+            "Email Address",
+            "Status",
+            "Start Time",
+            "Complete Time",
+            "Time Taken",
+            "Grade/20",
+        ]
+    ]
 
     return df
 
 
-def mark_submission_status(assignments, deadlines):
-    marked_assignments = pd.DataFrame()
+def clean_deadlines_csv(df: pd.DataFrame):
+    df["Assignment"] = df["Quizz type"] + " " + df["Quiz number"].astype(str)
+    df["Open Datetime"] = pd.to_datetime(
+        df["Open date"] + " " + df["Open time"],
+    )
+    df["Close Datetime"] = pd.to_datetime(df["Close date"] + " " + df["Close time"])
+    df.drop(
+        [
+            "Quizz type",
+            "Quiz number",
+            "Open date",
+            "Open time",
+            "Close date",
+            "Close time",
+        ],
+        axis=1,
+        inplace=True,
+    )
 
-    for index, row in deadlines.iterrows():
-        assignment_name = row["Quizz type"]
-        # assignment_close_time = row["close_datetime"]
+    return df
 
-        # Filter assignments for the specific assignment
-        assignment_submissions = assignments[
-            assignments["Quizz type"] == assignment_name
+
+# i want for each assignment to get the deadline close_date from my already created deadlines
+# dataframe called deadlines.  then, i want to check for each student in HW01.csv the time
+# they started the homework. then, i want to find the difference between when the assignment
+# opened and the time the student started the assignement. i want to then cross check this
+# difference with the early, middle, and late windows, effectively marking each student as early,
+#  middle, or late
+
+
+def mark_submission_status(
+    assignment: pd.DataFrame, deadlines: pd.DataFrame
+) -> (
+    pd.DataFrame
+):  # assignment is HW01 dataset, deadlines is test.csv (the deadlines dataset)
+    marked_assignments = pd.DataFrame(
+        columns=[
+            "Student ID",
+            "Assignment Name",
+            "Submission Status",
+            "Deadline Close Date",
         ]
+    )
 
-        for _, student_submission in assignment_submissions.iterrows():
-            student_id = student_submission["ID Number"]
-            student_start_time = student_submission["Started on"]
-            date_format = "%d %B %Y %I:%M %p"  # converting date
-            parsed_date = pd.to_datetime(student_start_time, format=date_format)
-            student_start_time = parsed_date.dt.time
-            print(student_start_time)
+    # in deadlines, find close datetime
+    for _, row in deadlines.iterrows():
+        assignment_type = row["Assignment"]
+        deadline_close_date = row["Close Datetime"]
+
+        # in HW01 dataset, find "Start Time"
+
+        for _, student_submission in assignment.iterrows():
+            student_id = student_submission["Student ID"]  # keep track of ID
+
+            student_start_time = pd.to_datetime(
+                student_submission["Start Time"].to_pydatetime()
+            )
 
             # Calculate the difference between assignment open time and student start time
             time_difference = (
-                student_start_time - row["open_datetime"]
+                student_start_time - row["Open Datetime"].to_pydatetime()
             ).total_seconds()
 
             # Mark students based on time difference
             if time_difference >= row["early_window"][1]:
-                status = "Early"
+                status = "Late"  # i know this part looks wrong, but i tested a lot and its right
             elif row["middle_window"][1] <= time_difference < row["middle_window"][0]:
                 status = "Middle"
             else:
-                status = "Late"
+                status = "Early"  # same here, naming is confusing but this correctly labels status of students
 
             # Append the marked assignment to the result DataFrame
-            marked_assignments = marked_assignments.append(
-                {
-                    "Student ID": student_id,
-                    "Assignment Name": assignment_name,
-                    "Submission Status": status,
-                },
+            marked_assignments = pd.concat(
+                [
+                    marked_assignments,
+                    pd.DataFrame(
+                        [[student_id, assignment_type, status, deadline_close_date]],
+                        columns=[
+                            "Student ID",
+                            "Assignment Name",
+                            "Submission Status",
+                            "Deadline Close Date",
+                        ],
+                    ),
+                ],
                 ignore_index=True,
             )
 
     return marked_assignments
 
 
-df = pd.read_csv("HW01.csv")
-df = clean_assignment_csv(df)
+def find_time_window(deadlines: pd.DataFrame) -> pd.DataFrame:
+    # Calculate the time window for each assignment
+    deadlines["time_window"] = deadlines.apply(
+        lambda row: row["Close Datetime"] - row["Open Datetime"], axis=1
+    )
 
+    # Convert days to hours and extract components (hours, minutes, seconds)
+    deadlines["hours"] = deadlines["time_window"].dt.total_seconds() / 3600
+    deadlines["minutes"] = deadlines["time_window"].dt.components["minutes"]
+    deadlines["seconds"] = deadlines["time_window"].dt.components["seconds"]
+
+    # Format the result as HH:MM:SS
+    deadlines["formatted_time"] = deadlines.apply(
+        lambda row: f"{int(row['hours']):02d}:{row['minutes']:02d}:{row['seconds']:02d}",
+        axis=1,
+    )
+
+    # Convert formatted time to total seconds
+    deadlines["total_seconds"] = pd.to_timedelta(
+        deadlines["formatted_time"]
+    ).dt.total_seconds()
+
+    # Calculate time windows for grouping
+    deadlines["early_window"] = deadlines["total_seconds"].apply(
+        lambda secs: calculate_windows(secs, "early")
+    )
+
+    deadlines["middle_window"] = deadlines["total_seconds"].apply(
+        lambda secs: calculate_windows(secs, "mid")
+    )
+
+    deadlines["late_window"] = deadlines["total_seconds"].apply(
+        lambda secs: calculate_windows(secs, "late")
+    )
+    return deadlines[["early_window", "middle_window", "late_window"]]
+
+
+# assignment = pd.read_csv("../../../analysis/merged_df_F20_hw.csv")
+assignment = pd.read_csv("HW01.csv")
+
+assignment = clean_assignment_csv(assignment)
+
+# deadlines = pd.read_excel("../../Moodle Datasets.xlsx")
 deadlines = pd.read_csv("test.csv")
-deadlines["open_datetime"] = pd.to_datetime(
-    deadlines["Open date"] + " " + deadlines["Open time"]
-)
-deadlines["close_datetime"] = pd.to_datetime(
-    deadlines["Close date"] + " " + deadlines["Close time"]
-)
+deadlines = clean_deadlines_csv(deadlines)
+windows = find_time_window(deadlines)
+marked_assignments = mark_submission_status(assignment, deadlines)
 
-# Calculate the time window for each assignment
-deadlines["time_window"] = deadlines.apply(
-    lambda row: row["close_datetime"] - row["open_datetime"], axis=1
-)
-
-
-# Convert days to hours and extract components (hours, minutes, seconds)
-deadlines["hours"] = deadlines["time_window"].dt.total_seconds() / 3600
-deadlines["minutes"] = deadlines["time_window"].dt.components["minutes"]
-deadlines["seconds"] = deadlines["time_window"].dt.components["seconds"]
-
-# Format the result as HH:MM:SS
-deadlines["formatted_time"] = deadlines.apply(
-    lambda row: f"{int(row['hours']):02d}:{row['minutes']:02d}:{row['seconds']:02d}",
-    axis=1,
-)
-
-# Convert formatted time to total seconds
-deadlines["total_seconds"] = pd.to_timedelta(
-    deadlines["formatted_time"]
-).dt.total_seconds()
-print(deadlines["total_seconds"])
-
-# Calculate time windows for grouping
-deadlines["early_window"] = deadlines["total_seconds"].apply(
-    lambda secs: calculate_windows(secs, "early")
-)
-
-deadlines["middle_window"] = deadlines["total_seconds"].apply(
-    lambda secs: calculate_windows(secs, "mid")
-)
-
-deadlines["late_window"] = deadlines["total_seconds"].apply(
-    lambda secs: calculate_windows(secs, "late")
-)
-
-# print(deadlines[["early_window", "middle_window", "late_window"]])
+# print(marked_assignments)
